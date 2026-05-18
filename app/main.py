05b9832803e0f3bc9c0e2d033ac45237d2e34cbf
jhojan <bgm5433@gmail.com>
2026-05-18 05:16:59 +0200

    feat: agregada interfaz visual html/css para el frontend de sigeh

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import os
 
-app = FastAPI(title="SIGEH API", description="Sistema Integral de Gestión Hospitalaria")
+app = FastAPI(title="SIGEH API")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"status": "ok", "message": "API de SIGEH funcionando correctamente en entorno Docker"}
+    # Buscamos la ruta exacta del archivo index.html
+    current_dir = os.path.dirname(__file__)
+    html_path = os.path.join(current_dir, "index.html")
+    
+    with open(html_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok", "message": "API de SIGEH funcionando correctamente"}
